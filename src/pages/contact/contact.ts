e0fc19c1d40620f8cc987d51ded6dbe0d7322944
abdullah <abdallh.fcih@gmail.com>
2018-03-02 17:25:22 +0200

    done with goole login but have issues with facebook login

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
 import { LoginPage } from '../login/login';
 import { RegisterPage } from '../register/register';
+import { HomePage } from '../home/home';
 
 @Component({
   selector: 'page-contact',
@@ -10,16 +14,30 @@ import { RegisterPage } from '../register/register';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public fireAuth: AngularFireAuth) {
   }
 
   logIn() {
     this.navCtrl.push(LoginPage);
   }
 
+  logOut() {
+    this.fireAuth.auth.signOut();
+    this.navCtrl.push(HomePage);
+  }
+
   register() {
     this.navCtrl.push(RegisterPage);
+  }
+
+  logInFacebook() {
+    this.fireAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider()).then(res => {console.log(res);
+    });
+  }
+
+  logInGoogle() {
+    this.fireAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(res => {console.log(res);
+    });
   }
 
 }
